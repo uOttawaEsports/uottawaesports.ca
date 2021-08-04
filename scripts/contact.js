475ea90d7c87a095709dac30afd0363d9f3e5e2f
Steven Li <stevenli.581@gmail.com>
2021-08-03 23:46:16 -0400

    Fixed contact layout for smaller screens

--- scripts/contact.js
+++ scripts/contact.js
@@ -9,13 +9,13 @@ function validateName(input) {
     let label = document.getElementById("nameLabel");
 
     if (input.value.length == 0) {
-        input.classList.remove("good");
+        input.classList.remove("valid");
         input.classList.add("error");
         label.innerHTML = '<i class="fas fa-exclamation-circle px-1 text-danger"></i> Name';
         return false;
     } else {
         input.classList.remove("error");
-        input.classList.add("good");
+        input.classList.add("valid");
         label.innerHTML = '<i class="fas fa-check-circle px-1 text-success"></i>  Name';
         return true;
     }
@@ -28,14 +28,14 @@ function validateDiscord(input) {
     const di = new RegExp("^.{3,32}#[0-9]{4}$");
 
     if (!di.test(input.value)) {
-        input.classList.remove("good");
+        input.classList.remove("valid");
         input.classList.add("error");
         label.innerHTML = '<i class="fas fa-exclamation-circle px-1 text-danger"></i> Discord&nbsp;Username';
         document.getElementById('discordTooltip').setAttribute('data-bs-original-title', 'Invalid Discord username. Format: Example#1234');
         return false;
     } else {
         input.classList.remove("error");
-        input.classList.add("good");
+        input.classList.add("valid");
         label.innerHTML = '<i class="fas fa-check-circle px-1 text-success"></i>  Discord&nbsp;Username';
         document.getElementById('emailTooltip').setAttribute('data-bs-original-title', 'Enter your Discord username.');
         return true;
@@ -50,34 +50,34 @@ function validateEmail(input) {
     const em = new RegExp("[\\w\\-\\.]+@[a-zA-Z0-9]+\\.[[a-zA-Z]+");
 
     if (!em.test(input.value)) {
-        input.classList.remove("good");
+        input.classList.remove("valid");
         input.classList.add("error");
         label.innerHTML = '<i class="fas fa-exclamation-circle px-1 text-danger"></i> Email&nbsp;Address';
         document.getElementById('emailTooltip').setAttribute('data-bs-original-title', 'Invalid email address. Format: example@domain.com');
         return false;
     } else {
         input.classList.remove("error");
-        input.classList.add("good");
+        input.classList.add("valid");
         label.innerHTML = '<i class="fas fa-check-circle px-1 text-success"></i> Email&nbsp;Address';
         document.getElementById('discordTooltip').setAttribute('data-bs-original-title', 'Enter your email address.');
         return true;
     }
 }
 
-/* Validate Type */
+/* Validate Reason */
 
-function validateType(input) {
-    let label = document.getElementById("typeLabel");
+function validateReason(input) {
+    let label = document.getElementById("reasonLabel");
 
     if (input.value.length == 0) {
-        input.classList.remove("good");
+        input.classList.remove("valid");
         input.classList.add("error");
-        label.innerHTML = '<i class="fas fa-exclamation-circle px-1 text-danger"></i> Enquiry&nbsp;Type';
+        label.innerHTML = '<i class="fas fa-exclamation-circle px-1 text-danger"></i> Contact&nbsp;Reason';
         return false;
     } else {
         input.classList.remove("error");
-        input.classList.add("good");
-        label.innerHTML = '<i class="fas fa-check-circle px-1 text-success"></i>  Enquiry&nbsp;Type';
+        input.classList.add("valid");
+        label.innerHTML = '<i class="fas fa-check-circle px-1 text-success"></i>  Contact&nbsp;Reason';
         return true;
     }
 }
@@ -88,14 +88,14 @@ function validateSubject(input) {
     let label = document.getElementById("subjectLabel");
 
     if (input.value.length == 0) {
-        input.classList.remove("good");
+        input.classList.remove("valid");
         input.classList.add("error");
         label.innerHTML = '<i class="fas fa-exclamation-circle px-1 text-danger"></i> Subject';
         document.getElementById('subjectTooltip').setAttribute('data-bs-original-title', 'Enter the subject. Example: Discord role change');
         return false;
     } else {
         input.classList.remove("error");
-        input.classList.add("good");
+        input.classList.add("valid");
         label.innerHTML = '<i class="fas fa-check-circle px-1 text-success"></i>  Subject';
         document.getElementById('subjectTooltip').setAttribute('data-bs-original-title', 'Enter the subject.');
         return true;
@@ -108,13 +108,13 @@ function validateMessage(input) {
     let label = document.getElementById("message");
 
     if (input.value.length == 0) {
-        input.classList.remove("good");
+        input.classList.remove("valid");
         input.classList.add("error");
         label.placeholder = 'Please tell us how we can help you!';
         return false;
     } else {
         input.classList.remove("error");
-        input.classList.add("good");
+        input.classList.add("valid");
         label.placeholder = 'How can we help you?';
         return true;
     }
@@ -124,7 +124,7 @@ function validate() {
     let name = document.getElementById("name");
     let discord = document.getElementById("discord");
     let email = document.getElementById("email");
-    let type = document.getElementById("type");
+    let reason = document.getElementById("reason");
     let subject = document.getElementById("subject");
     let message = document.getElementById("message");
     let valid = true;
@@ -135,7 +135,7 @@ function validate() {
     if (!validateEmail(email)) {
         valid = false;
     }
-    if (!validateType(type)) {
+    if (!validateReason(reason)) {
         valid = false;
     }
     if (!validateSubject(subject)) {
