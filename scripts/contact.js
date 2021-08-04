b9d803169204274034f3450e6d419496358a1dc5
Steven Li <stevenli.581@gmail.com>
2021-08-03 20:26:03 -0400

    Updated contact form

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,34 +3,18 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-/* Validate First Name */
+/* Validate Name */
 
-function validateFirst(input) {
-    let label = document.getElementById("firstLabel");
-
-    if (input.value.length == 0) {
-        input.classList.add("error");
-        label.innerHTML = '<i class="fas fa-exclamation-circle px-1 text-danger"></i> First Name';
-        return false;
-    } else {
-        input.classList.remove("error");
-        label.innerHTML = 'First Name';
-        return true;
-    }
-}
-
-/* Validate Last Name */
-
-function validateLast(input) {
-    let label = document.getElementById("lastLabel");
+function validateName(input) {
+    let label = document.getElementById("nameLabel");
 
     if (input.value.length == 0) {
         input.classList.add("error");
-        label.innerHTML = '<i class="fas fa-exclamation-circle px-1 text-danger"></i> Last Name';
+        label.innerHTML = '<i class="fas fa-exclamation-circle px-1 text-danger"></i> Name';
         return false;
     } else {
         input.classList.remove("error");
-        label.innerHTML = 'Last Name';
+        label.innerHTML = 'Name';
         return true;
     }
 }
@@ -44,14 +28,12 @@ function validateEmail(input) {
     if (!em.test(input.value)) {
         input.classList.add("error");
         label.innerHTML = '<i class="fas fa-exclamation-circle px-1 text-danger"></i> Email Address';
-        document.getElementById('emailTooltip').setAttribute('title', 'Invalid email address. Format: example@domain.com Example: adam@hotmail.com');
-        document.getElementById('emailTooltip').setAttribute('data-bs-original-title', 'Invalid email address. Format: example@domain.com Example: adam@hotmail.com');
+        document.getElementById('emailTooltip').setAttribute('data-bs-original-title', 'Invalid email address. Format: example@domain.com');
         return false;
     } else {
         input.classList.remove("error");
         label.innerHTML = 'Email Address';
-        document.getElementById('emailTooltip').setAttribute('title', 'Email Address');
-        document.getElementById('emailTooltip').setAttribute('data-bs-original-title', 'Email Address');
+        document.getElementById('emailTooltip').setAttribute('data-bs-original-title', 'Enter your email address.');
         return true;
     }
 }
@@ -80,19 +62,17 @@ function validateSubject(input) {
     if (input.value.length == 0) {
         input.classList.add("error");
         label.innerHTML = '<i class="fas fa-exclamation-circle px-1 text-danger"></i> Subject';
-        document.getElementById('subjectTooltip').setAttribute('title', 'Enter the subject. Example: Discord role change');
         document.getElementById('subjectTooltip').setAttribute('data-bs-original-title', 'Enter the subject. Example: Discord role change');
         return false;
     } else {
         input.classList.remove("error");
         label.innerHTML = 'Subject';
-        document.getElementById('subjectTooltip').setAttribute('title', 'Enter the subject.');
         document.getElementById('subjectTooltip').setAttribute('data-bs-original-title', 'Enter the subject.');
         return true;
     }
 }
 
-/* Validate First Name */
+/* Validate Message */
 
 function validateMessage(input) {
     let label = document.getElementById("message");
@@ -109,32 +89,17 @@ function validateMessage(input) {
 }
 
 function validate() {
-    let firstName = document.getElementById("fname");
-    let lastName = document.getElementById("lname");
+    let name = document.getElementById("name");
     let email = document.getElementById("email");
-    let phone = document.getElementById("phone");
-    let language = document.getElementById("language");
     let type = document.getElementById("type");
     let subject = document.getElementById("subject");
-    let ccName = document.getElementById("cardName");
-    let ccNumber = document.getElementById("ccNumber");
-    let ccExpiry = document.getElementById("ccExpiry");
-    let ccCVV = document.getElementById("ccCVV");
+    let message = document.getElementById("message");
     let valid = true;
 
-    if (!validateFirst(firstName)) {
-        valid = false;
-    }
-    if (!validateLast(lastName)) {
+    if (!validateName(name)) {
         valid = false;
     }
     if (!validateEmail(email)) {
-        valid = false;
-    }
-    if (!validatePhone(phone)) {
-        valid = false;
-    }
-    if (!validateLanguage(language)) {
         valid = false;
     }
     if (!validateType(type)) {
@@ -143,16 +108,7 @@ function validate() {
     if (!validateSubject(subject)) {
         valid = false;
     }
-    if (!validateName(ccName)) {
-        valid = false;
-    }
-    if (!validateCCNumber(ccNumber)) {
-        valid = false;
-    }
-    if (!validateExpiry(ccExpiry)) {
-        valid = false;
-    }
-    if (!validateCVV(ccCVV)) {
+    if (!validateMessage(message)) {
         valid = false;
     }
 
