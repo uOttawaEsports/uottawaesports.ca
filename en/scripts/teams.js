32be9f82478113f405003d1dc0b8db6d20e4f5b9
Steven Li <stevenli.581@gmail.com>
2021-08-17 20:26:26 -0400

    Added teams buttons and cleaned code

---
+++ en/scripts/teams.js
@@ -0,0 +1,138 @@
+const modals = {
+    "csgo garnet": {
+        title: "Valorant Garnet Team",
+        background: "#800020",
+        members: {
+
+        }
+    },
+    "csgo grey": {
+        title: "Valorant Grey Team",
+        background: "#303031",
+        members: {
+
+        }
+    },
+    "dota2 garnet": {
+        title: "Valorant Garnet Team",
+        background: "#800020",
+        members: {
+
+        }
+    },
+    "dota2 grey": {
+        title: "Valorant Grey Team",
+        background: "#303031",
+        members: {
+
+        }
+    },
+    "lol garnet": {
+        title: "Valorant Garnet Team",
+        background: "#800020",
+        members: {
+
+        }
+    },
+    "lol grey": {
+        title: "Valorant Grey Team",
+        background: "#303031",
+        members: {
+
+        }
+    },
+    "r6 garnet": {
+        title: "Valorant Garnet Team",
+        background: "#800020",
+        members: {
+
+        }
+    },
+    "r6 grey": {
+        title: "Valorant Grey Team",
+        background: "#303031",
+        members: {
+
+        }
+    },
+    "rl garnet": {
+        title: "Valorant Garnet Team",
+        background: "#800020",
+        members: {
+
+        }
+    },
+    "rl grey": {
+        title: "Valorant Grey Team",
+        background: "#303031",
+        members: {
+
+        }
+    },
+    "val garnet": {
+        title: "Valorant Garnet Team",
+        background: "#800020",
+        members: {
+            "Kiro": "Manager",
+            "kang xd": "Coach",
+            "bladee": "Main",
+            "coal": "Main",
+            "housieee": "Main",
+            "Jacob": "Main",
+            "Jer": "Main",
+            "Apl": "Substitute",
+            "SyCoLight": "Substitute",
+        }
+    },
+    "val grey": {
+        title: "Valorant Grey Team",
+        background: "#303031",
+        members: {
+            "Econ God": "Grey",
+            "chriski": "Main",
+            "Goff": "Main",
+            "inertia": "Main",
+            "Kiro": "Main",
+            "Leek": "Main",
+            "LittleBozz": "Substitute",
+        }
+    }
+}
+
+function teams(name) {
+
+    document.getElementById("modalTitle").innerHTML = modals[name].title;
+    document.getElementById("modalBackground").style.backgroundColor = modals[name].background;
+
+    let body = document.getElementById("modalBody");
+    body.innerHTML = "";
+
+    for (let member in modals[name]["members"]) {
+        let pos = modals[name]["members"][member];
+        let tr = document.createElement('tr');
+        let td = document.createElement('td');
+        td.innerHTML = member;
+        tr.appendChild(td);
+        td = document.createElement('td');
+        td.innerHTML = pos;
+        tr.appendChild(td);
+        body.appendChild(tr);
+    }
+}
+
+const info = document.getElementById("information");
+const btn = document.getElementById("information-button");
+
+btn.onclick = function () {
+    if (info.style.display === "none") {
+        info.style.display = "block";
+    } else {
+        info.style.display = "none";
+    }
+};
+
+window.addEventListener('click', function (e) {
+    if (!info.contains(e.target) && !btn.contains(e.target)) {
+        info.style.display = "none";
+    }
+});
