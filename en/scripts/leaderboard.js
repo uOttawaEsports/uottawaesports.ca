6c2afbff1929d470318e19635041fa9af33b7d60
Steven Li <stevenli.581@gmail.com>
2021-08-20 22:28:24 -0400

    Added leaderboard styling

--- en/scripts/leaderboard.js
+++ en/scripts/leaderboard.js
@@ -1,26 +1,26 @@
 let people = { // keys are unique IDs
     "0": {
-        name: "",
-        pts: 100
+        name: "Steven Li",
+        pts: 1100
     },
     "1": {
-        name: "",
-        pts: 90
+        name: "Lorem Ipsum",
+        pts: 190
     },
     "2": {
-        name: "",
+        name: "Lorem Ipsum",
         pts: 80
     },
     "3": {
-        name: "",
+        name: "Lorem Ipsum",
         pts: 1000
     },
     "4": {
-        name: "",
-        pts: 95
+        name: "Lorem Ipsum",
+        pts: 195
     },
     "5": {
-        name: "Harnza",
+        name: "Lorem Ipsum",
         pts: -1
     }
 }
@@ -29,51 +29,69 @@ let sorted = Object.keys(people).sort((a, b) => { return people[b].pts - people[
 
 let pos = 1;
 let headerCreated = false;
+
 sorted.forEach(personObj => {
     if (pos <= 3) {
-        let div1 = document.createElement("div");
-        div1.className = "d-flex border mb-4";
+        let div = document.createElement("div");
+        div.className = "d-flex mb-4";
+        div.className += (pos == 1 ? " bg-gold" : pos == 2 ? " bg-silver" : " bg-bronze")
         let img = document.createElement("img");
-        img.src = "../../images/leaderboard/" + (pos == 1 ? "gold" : pos == 2 ? "silver" : "bronze") + ".png"
-        img.alt = (pos == 1 ? "gold" : pos == 2 ? "silver" : "bronze")
+        img.src = "../../images/leaderboard/" + (pos == 1 ? "gold" : pos == 2 ? "silver" : "bronze") + ".png";
+        img.alt = (pos == 1 ? "Gold badge" : pos == 2 ? "Silver badge" : "Bronze badge");
+        img.className = "p-1";
         img.style.height = "5rem";
         img.style.width = "5rem";
+        div.appendChild(img);
+        let name = document.createElement("p");
+        name.className = "fs-3 d-flex align-self-center me-auto mb-0 ms-1";
+        name.innerHTML = ('<span class="fw-bold">' + pos + ".</span>&nbsp;" + people[personObj].name);
+        div.appendChild(name);
+        let points = document.createElement("p");
+        points.className = "fs-3 d-flex align-self-center mb-0 me-2";
+        points.innerHTML = people[personObj].pts + " points";
+        div.appendChild(points);
+        document.getElementById("top3").appendChild(div);
         pos++;
-        div1.appendChild(img)
-        let div2 = document.createElement("div");
-        div2.innerHTML = people[personObj].name;
-        div1.appendChild(div2);
-        div2 = document.createElement("div");
-        div2.innerHTML = people[personObj].pts + "pts";
-        div1.appendChild(div2);
-        document.getElementById("top3").appendChild(div1);
     } else {
         if (!headerCreated) {
             let tr = document.createElement("tr");
             let th = document.createElement("th");
+
             th.scope = "col";
             th.innerHTML = "Position";
+            th.className = "text-center"
+            th.style.width = "1%";
+            th.style.whiteSpace = "nowrap";
             tr.appendChild(th);
             th = document.createElement("th");
             th.scope = "col";
             th.innerHTML = "Name";
+            th.className = "w-50";
             tr.appendChild(th);
             th = document.createElement("th");
             th.scope = "col";
             th.innerHTML = "Points";
+            th.className = "text-center";
+            th.style.width = "1%";
+            th.style.whiteSpace = "nowrap";
             tr.appendChild(th);
             document.getElementById("table-head").appendChild(tr);
             headerCreated = true;
         }
         let tr = document.createElement("tr");
+        let th = document.createElement("th");
         let td = document.createElement("td");
-        td.innerHTML = pos++;
-        tr.appendChild(td);
+        
+        th.innerHTML = pos++;
+        th.scope = "row";
+        th.className = "text-center";
+        tr.appendChild(th);
         td = document.createElement("td");
         td.innerHTML = people[personObj].name;;
         tr.appendChild(td);
         td = document.createElement("td");
         td.innerHTML = people[personObj].pts;
+        td.className = "text-end";
         tr.appendChild(td);
         document.getElementById("table-body").appendChild(tr);
     }
