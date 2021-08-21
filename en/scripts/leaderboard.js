5983f8f17dd0e91d0ed41516cb3fe3cd09bae3d7
Steven Li <stevenli.581@gmail.com>
2021-08-20 22:30:42 -0400

    Cleaned code

--- en/scripts/leaderboard.js
+++ en/scripts/leaderboard.js
@@ -33,24 +33,29 @@ let headerCreated = false;
 sorted.forEach(personObj => {
     if (pos <= 3) {
         let div = document.createElement("div");
-        div.className = "d-flex mb-4";
-        div.className += (pos == 1 ? " bg-gold" : pos == 2 ? " bg-silver" : " bg-bronze")
         let img = document.createElement("img");
+        let name = document.createElement("p");
+        let points = document.createElement("p");
+
+        div.className = "d-flex mb-4";
+        div.className += (pos == 1 ? " bg-gold" : pos == 2 ? " bg-silver" : " bg-bronze");
+
         img.src = "../../images/leaderboard/" + (pos == 1 ? "gold" : pos == 2 ? "silver" : "bronze") + ".png";
         img.alt = (pos == 1 ? "Gold badge" : pos == 2 ? "Silver badge" : "Bronze badge");
         img.className = "p-1";
         img.style.height = "5rem";
         img.style.width = "5rem";
         div.appendChild(img);
-        let name = document.createElement("p");
+
         name.className = "fs-3 d-flex align-self-center me-auto mb-0 ms-1";
         name.innerHTML = ('<span class="fw-bold">' + pos + ".</span>&nbsp;" + people[personObj].name);
         div.appendChild(name);
-        let points = document.createElement("p");
+
         points.className = "fs-3 d-flex align-self-center mb-0 me-2";
         points.innerHTML = people[personObj].pts + " points";
         div.appendChild(points);
         document.getElementById("top3").appendChild(div);
+
         pos++;
     } else {
         if (!headerCreated) {
@@ -63,11 +68,13 @@ sorted.forEach(personObj => {
             th.style.width = "1%";
             th.style.whiteSpace = "nowrap";
             tr.appendChild(th);
+
             th = document.createElement("th");
             th.scope = "col";
             th.innerHTML = "Name";
             th.className = "w-50";
             tr.appendChild(th);
+
             th = document.createElement("th");
             th.scope = "col";
             th.innerHTML = "Points";
@@ -75,24 +82,28 @@ sorted.forEach(personObj => {
             th.style.width = "1%";
             th.style.whiteSpace = "nowrap";
             tr.appendChild(th);
-            document.getElementById("table-head").appendChild(tr);
+
+            document.getElementById("tableHead").appendChild(tr);
             headerCreated = true;
         }
         let tr = document.createElement("tr");
         let th = document.createElement("th");
         let td = document.createElement("td");
-        
+
         th.innerHTML = pos++;
         th.scope = "row";
         th.className = "text-center";
         tr.appendChild(th);
+
         td = document.createElement("td");
         td.innerHTML = people[personObj].name;;
         tr.appendChild(td);
+
         td = document.createElement("td");
         td.innerHTML = people[personObj].pts;
         td.className = "text-end";
         tr.appendChild(td);
-        document.getElementById("table-body").appendChild(tr);
+
+        document.getElementById("tableBody").appendChild(tr);
     }
 });
