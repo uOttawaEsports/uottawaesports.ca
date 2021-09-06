88a39de553cea09d8ade0cf455bd4a35ba341036
Steven Li <stevenli.581@gmail.com>
2021-09-06 16:53:33 -0400

    Finished gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,73 +1,21 @@
-const images = {
-    0 : {
-        src: "../../images/clubs-week.jpg",
-        alt: "clubs-week",
-    },
-    1 : {
-        src: "../../images/community-for-all.jpg",
-        alt: "community-for-all",
-    },
-    2 : {
-        src: "../../images/meet-and-greet.jpg",
-        alt: "meet-and-greet",
-    },
-    3 : {
-        src: "../../images/gaming-session.jpg",
-        alt: "gaming-session",
-    },
-    4 : {
-        src: "../../images/smash-gathering.jpg",
-        alt: "smash-gathering",
-    },
-    5 : {
-        src: "../../images/taberet-hall.jpg",
-        alt: "taberet-hall",
-    },
-    6 : {
-        src: "../../images/uottawa-background.jpg",
-        alt: "uottawa-background",
-    },
-};
+// Add newest images to the end of the array.
+const images = ["../../images/clubs-week.jpg", 
+                "../../images/community-for-all.jpg", 
+                "../../images/meet-and-greet.jpg",
+                "../../images/gaming-session.jpg",
+                "../../images/smash-gathering.jpg"
+];
 
 let content = document.getElementById("content");
-for (let image in images) {
+for(let i = images.length-1; i >= 0; i--){
     let div = document.createElement("div");
-    let img = document.createElement("img");
-    let img2 = document.createElement("img");
-    img.src = images[image].src;
-    img.alt = images[image].alt;
-    img2.src = images[image].src;
-    img2.alt = images[image].alt;
-    div.className = "m-3 round";
-    div.appendChild(img);
-
-
+    div.className = "m-2 rounded";
     div.style.width = "21.875rem";
     div.style.height = "15.625rem";
-    img.style.height = "100%";
-    img.style.width = "100%";
-    img2.style.height = "100%";
-    img2.style.width = "100%";
-
-    div.setAttribute("data-bs-toggle", "modal");
-    div.setAttribute("data-bs-target", "#modal");
-
-    div.addEventListener("click", function(){ 
-        //document.getElementById("modalBackground").style = `background-image: url(${images[image].src}); background-position-x: 50%; background-position-y: 50%;` 
-        let modal = document.getElementById("modal-body");
-        modal.innerHTML = "";
-        modal.appendChild(img2);
-    });
-
-    /*div.addEventListener("click", function(){ 
-        let modal = document.getElementById("modal");
-        modal.innerHTML = "";
-        modal.appendChild(img2);
-        modal.style.display = "inline";
-    });*/
-    // img.style.backgroundRepeat = "no-repeat";
-    // img.style.backgroundSize = "cover";
-    // img.style.backgroundPositionX = "50%";
-    // img.style.backgroundPositionY = "50%";
+    div.style.backgroundImage = "url(" + images[i] + ")";
+    div.style.backgroundRepeat = "no-repeat";
+    div.style.backgroundSize = "cover";
+    div.style.backgroundPositionX = "50%";
+    div.style.backgroundPositionY = "50%";
     content.appendChild(div);
 }
