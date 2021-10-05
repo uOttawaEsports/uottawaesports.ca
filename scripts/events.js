d55ee9f1b6fda33dcb5af3171a6bd1b9a3417c6b
Steven Li <stevenli.581@gmail.com>
2021-10-04 21:02:16 -0400

    Updated events

--- scripts/events.js
+++ scripts/events.js
@@ -20,15 +20,15 @@ const events = [
     //     location: ["<a href='https://www.twitch.tv/uottawaesports' rel='external' target='_blank'>Twitch</a>", "<a href='https://www.twitch.tv/uottawaesports' rel='external' target='_blank'>Twitch</a>"],
     //     contact: ["Yell#8888", "Yell#8888"]
     // },
-    {
-        title: ["VALORANT In-house", "In-house de VALORANT"],
-        image: ["../../images/events/upcoming/val2.png", "VALORANT logo", "Logo de VALORANT"],
-        text: ["Introducing the first ever prized in-house! More information can be found on Discord.<br/><span class='fw-bold'>Prizes:</span> 1st place: $75 split evenly | 2nd place: $25 split evenly <hr />You must fill in the two following forms to participate: <a href='https://docs.google.com/forms/d/e/1FAIpQLScxf03sVCgAm5ush6zFBXXZ6oDsrM58WuFu6fIFxy8O5le4YA/viewform' rel='external' target='_blank'>Tournament Registration</a> and <a href='https://bit.ly/3BXwrWm' rel='external' target='_blank'>VALORANT Member Form</a>.", "Le tout premier «&nbsp;in-house&nbsp;» avec un prix se passe ce dimanche&nbsp;! Plus de renseignements sont disponibles sur Discord.<br/><span class='fw-bold'>Les prix:</span> 1ère place&nbsp;: 75&nbsp;$ divisé également | 2ème place&nbsp;: 25&nbsp;$ divisé également <hr />Pour participer, vous devez remplir les deux formulaires suivants&nbsp;: <a href='https://docs.google.com/forms/d/e/1FAIpQLScxf03sVCgAm5ush6zFBXXZ6oDsrM58WuFu6fIFxy8O5le4YA/viewform' rel='external' target='_blank'>Inscription au tournoi</a> et <a href='https://bit.ly/3BXwrWm' rel='external' target='_blank'>Formulaire d'adhésion de VALORANT</a>."],
-        date: ["Sunday, October 3", "Dimanche 3 octobre"],
-        time: ["5:00 PM", "17h00"],
-        location: ["Discord", "Discord"],
-        contact: ["onmarti#4347, Kiro#3697", "onmarti#4347, Kiro#3697"]
-    },
+    // {
+    //     title: ["VALORANT In-house", "In-house de VALORANT"],
+    //     image: ["../../images/events/upcoming/val2.png", "VALORANT logo", "Logo de VALORANT"],
+    //     text: ["Introducing the first ever prized in-house! More information can be found on Discord.<br/><span class='fw-bold'>Prizes:</span> 1st place: $75 split evenly | 2nd place: $25 split evenly <hr />You must fill in the two following forms to participate: <a href='https://docs.google.com/forms/d/e/1FAIpQLScxf03sVCgAm5ush6zFBXXZ6oDsrM58WuFu6fIFxy8O5le4YA/viewform' rel='external' target='_blank'>Tournament Registration</a> and <a href='https://bit.ly/3BXwrWm' rel='external' target='_blank'>VALORANT Member Form</a>.", "Le tout premier «&nbsp;in-house&nbsp;» avec un prix se passe ce dimanche&nbsp;! Plus de renseignements sont disponibles sur Discord.<br/><span class='fw-bold'>Les prix:</span> 1ère place&nbsp;: 75&nbsp;$ divisé également | 2ème place&nbsp;: 25&nbsp;$ divisé également <hr />Pour participer, vous devez remplir les deux formulaires suivants&nbsp;: <a href='https://docs.google.com/forms/d/e/1FAIpQLScxf03sVCgAm5ush6zFBXXZ6oDsrM58WuFu6fIFxy8O5le4YA/viewform' rel='external' target='_blank'>Inscription au tournoi</a> et <a href='https://bit.ly/3BXwrWm' rel='external' target='_blank'>Formulaire d'adhésion de VALORANT</a>."],
+    //     date: ["Sunday, October 3", "Dimanche 3 octobre"],
+    //     time: ["5:00 PM", "17h00"],
+    //     location: ["Discord", "Discord"],
+    //     contact: ["onmarti#4347, Kiro#3697", "onmarti#4347, Kiro#3697"]
+    // },
     {
         title: ["Annual General Meeting", "Assemblée générale annuelle"],
         image: ["../../images/events/upcoming/mnt-203.png", "MNT 203", "MNT 203"],
